07d9ccf5cb8a46041fbd13e22c8d196d15c3438e
Annika Sigfridsson <sigfridsson.annika@gmail.com>
2024-02-13 13:44:01 +0100

    Co-authored-by: Maja Schouenke <schouenkes@users.noreply.github.com> Co-authored-by: Johanna Catalini Smith <johannacatalinismith@users.noreply.github.com> Co-authored-by: Paulajungaker <Paulajungaker@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 console.log("Start Here 😉");
+// DOM selector
+const box = document.getElementsByClassName("box")
+
+// Declared function   
+const switchColor = () => {
+  
+if (input = dark) {
+    
+}
+
+}
+
