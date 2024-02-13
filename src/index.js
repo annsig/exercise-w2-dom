008adc6923af97f4091e586135ffbf70e68a5865
Annika Sigfridsson <sigfridsson.annika@gmail.com>
2024-02-13 14:46:07 +0100

    Changes made

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 console.log("Start Here 😉");
 // DOM selector
-const box = document.getElementsByClassName("box")
+const box = document.querySelector(".box")
 
-// Declared function   
-const switchColor = () => {
-  
-if (input = dark) {
+// const darkColor = document.get
+//     // Declared function   
+//     const switchColor = () => {
     
-}
+//     if (input = dark) {
+//     return "a" 
+//     }
 
-}
 
+//myButton.onclick = () => updateCount()
+
+const dark = document.querySelector('#dark')
+dark.addEventListener("click", () => {
+    box.style.backgroundColor = 'black'
+})
+
+// btn.onclick = () => {
+    //btn.style.backgroundColor = "black"
+    //btn.style.color = "yellow"
+//}
+//btn.onclick = () => {
+    //btn.classList.toggle("btn-dark")
+    //headerTitle.style.display = "none"
+//}
